914567e629aaa861f5a48613273b37f54511d70f
thienth32 <thienth32@gmail.com>
2019-04-03 15:30:12 +0700

    validate form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +20,7 @@ import { PublisherAddFormComponent } from './screens/publisher-add-form/publishe
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
--- src/app/screens/publisher-add-form/publisher-add-form.component.ts
+++ src/app/screens/publisher-add-form/publisher-add-form.component.ts
@@ -2,40 +2,51 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router"
 import {PublisherService} from '../../services/publisher.service';
 
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-publisher-add-form',
   templateUrl: './publisher-add-form.component.html',
   styleUrls: ['./publisher-add-form.component.css']
 })
 export class PublisherAddFormComponent implements OnInit {
-  
-  name: String;
-  avatar: String;
-  address: String;
-  description: String;
 
+  createForm: FormGroup;
   constructor(private pbsService: PublisherService,
               private router: Router) { }
 
   ngOnInit() {
+    this.addValidateForm();
+  }
+
+  addValidateForm(){
+    this.createForm = new FormGroup({
+  		name: new FormControl('thienth dep trai', [
+  			Validators.required,
+  			Validators.minLength(4)
+  		]),
+  		avatar: new FormControl('', Validators.required),
+  		address: new FormControl('', Validators.required),
+  		description: new FormControl('')
+  	});
   }
 
   saveForm(){
     // lay ra du lieu dc dien vao tu form
-    let data = {
-      name: this.name, 
-      avatar: this.avatar, 
-      address: this.address, 
-      description: this.description
-    };
+    // let data = {
+    //   name: this.name, 
+    //   avatar: this.avatar, 
+    //   address: this.address, 
+    //   description: this.description
+    // };
 
     // gui request len api de luu tru
-    this.pbsService.addPublisher(data).subscribe(data => {
-      if(data.id != undefined){
-        // neu gui thanh cong thi dieu huong ve trang danh sach
-        this.router.navigate(['/'])
-      }
-    });
+    // this.pbsService.addPublisher(data).subscribe(data => {
+    //   if(data.id != undefined){
+    //     // neu gui thanh cong thi dieu huong ve trang danh sach
+    //     this.router.navigate(['/'])
+    //   }
+    // });
   }
 
 }
